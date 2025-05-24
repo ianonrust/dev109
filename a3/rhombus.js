faca258fcc09aab2f87e6bf9a852297a4c0115a6
ianonrust <166336657+ianonrust@users.noreply.github.com>
2025-05-24 08:56:52 -0700

    Implemented rhombus javascript

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -1,1 +1,51 @@
-
+//Creating the rhombus in one function, this way we can have every color alternating throughout the diamond, not just in each quarter of it.
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+  const counter = { value: 0 };
+  let rhombusHTML = "";
+  
+  // Upper half of rhombus
+  for (let i = 0; i < pHeight; i++) {
+    rhombusHTML += "<div style='font-family: monospace; line-height: 1;'>";
+    rhombusHTML += "&nbsp;".repeat(pHeight - i - 1);
+    
+    // Left triangle (upLeft)
+    for (let j = 0; j <= i; j++) {
+      const color = (counter.value % 2 === 0) ? pColorOdd : pColorEven;
+      rhombusHTML += `<span style='color:${color};'>${pSymbol}</span>`;
+      counter.value++;
+    }
+    
+    // Right triangle (upRight)
+    for (let j = 0; j < i; j++) {
+      const color = (counter.value % 2 === 0) ? pColorOdd : pColorEven;
+      rhombusHTML += `<span style='color:${color};'>${pSymbol}</span>`;
+      counter.value++;
+    }
+    
+    rhombusHTML += "</div>";
+  }
+  
+  // Lower half of rhombus
+  for (let i = pHeight - 2; i >= 0; i--) {
+    rhombusHTML += "<div style='font-family: monospace; line-height: 1;'>";
+    rhombusHTML += "&nbsp;".repeat(pHeight - i - 1);
+    
+    // Left triangle (downLeft)
+    for (let j = 0; j <= i; j++) {
+      const color = (counter.value % 2 === 0) ? pColorOdd : pColorEven;
+      rhombusHTML += `<span style='color:${color};'>${pSymbol}</span>`;
+      counter.value++;
+    }
+    
+    // Right triangle (downRight)
+    for (let j = 0; j < i; j++) {
+      const color = (counter.value % 2 === 0) ? pColorOdd : pColorEven;
+      rhombusHTML += `<span style='color:${color};'>${pSymbol}</span>`;
+      counter.value++;
+    }
+    
+    rhombusHTML += "</div>";
+  }
+  
+  document.getElementById("rhombus").innerHTML = rhombusHTML;
+}
